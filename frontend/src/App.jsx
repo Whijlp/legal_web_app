180567e972790e24d18bcaf956849f2ff080496d
unknown <whijlp@outlook.com>
2025-06-19 20:36:41 -0500

    Rutas de login y landing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import Separador from "./components/Separador";
 import Landing from "./components/landing";
+import Login from "./components/login";
+import { Route, Routes } from "react-router";
 
 function App() {
   return (
@@ -9,7 +11,10 @@ function App() {
       <div className="page-stripes"></div>
       <Header />
       <Separador />
-      <Landing />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
 
       <Footer />
     </div>
