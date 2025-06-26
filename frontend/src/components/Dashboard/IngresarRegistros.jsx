c8a9f7e71a0ea050d463d99ddea92676dd124251
unknown <whijlp@outlook.com>
2025-06-25 21:05:26 -0500

    componentes Tab

--- frontend/src/components/Dashboard/IngresarRegistros.jsx
+++ frontend/src/components/Dashboard/IngresarRegistros.jsx
@@ -1,33 +1,66 @@
+import { useState } from "react";
+import TabContainer from "./TabContainer/TabContainer";
+
+
 function IngresarRegistros() {
+  const [showTab, setShowTab] = useState(false);
+
+  const handleShowTab = (e) => {
+    e.preventDefault();
+    setShowTab(prev => !prev);}
+
   return (
     <div className="registros">
-      <h1 className="registros_title">Ingresar y modificar Registros</h1>
-      <form className="registros_form">
-        <div className="registros_form-group">
-        <input
-        className="registros_form-item"
-          type="text"
-          id="nombre"
-          name="nombre"
-          placeholder="Buscar accionanate"
-        />
-        <button className="registros_form-button"
-        id="nombre_button">Buscar</button>
-        </div>
-        <div className="">
-               <input className="registros_form-item"
-          type="text"
-          id="radicado"
-          name="radicado"
-          placeholder="Buescar por numero radicado"
-        />
-        <button className="registros_form-button"
-        id="radicado_button">Buscar</button>
-        </div>
+      
+      
+     {!showTab ? (
+      <>
+        <h1 className="registros_title">Ingresar y modificar Registros</h1>
+        <form className="registros_form">
+          <div className="registros_form-group">
+            <input
+              className="registros_form-item"
+              type="text"
+              id="nombre"
+              name="nombre"
+              placeholder="Buscar accionante"
+            />
+            <button className="registros_form-button" type="button" id="nombre_button">Buscar</button>
+          </div>
+          <div>
+            <input
+              className="registros_form-item"
+              type="text"
+              id="radicado"
+              name="radicado"
+              placeholder="Buscar por número radicado"
+            />
+            <button className="registros_form-button" type="button" id="radicado_button">Buscar</button>
+          </div>
 
-         <button className="registros_form-button">Crear registros</button>
-      </form>
+          <button
+            type="button"
+            className="registros_form-button crear_registro"
+            onClick={handleShowTab}
+          >
+            Crear registros
+          </button>
+        </form>
+      </>
+    ) : (
+      <>
+      <TabContainer />
+        <button
+          type="button"
+          className="registros_form-button cerrar_registro"
+          onClick={handleShowTab}
+        >
+          Cerrar
+        </button>
+        
+      </>
+    )}
     </div>
   );
-}
+}""
 export default IngresarRegistros;
---
+++ frontend/src/components/Dashboard/TabContainer/TabContainer.jsx
@@ -0,0 +1,9 @@
+function TabContainer() {
+  return (
+    <div className="tabContainer">
+      
+      <p>Hola </p>
+    </div>
+  );
+}
+export  default TabContainer;
